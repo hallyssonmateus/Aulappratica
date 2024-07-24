173b9e0cefdfcbb364ff0d4d7b965e29a792ade8
hallyssonmateus <hallyssonmateus@gmail.com>
2024-07-24 12:27:38 -0300

    Adicionando msg de Hello World em c#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string mensagemDeBoasVindas = "Bem vindos a minha aula basica de C#";
+
+void ExibirMensagemBoasVindas(){
+    Console.WriteLine("*********************");
+    Console.WriteLine(mensagemDeBoasVindas);
+    Console.WriteLine("*********************");
+}
+
+
+ExibirMensagemBoasVindas();
+
+
